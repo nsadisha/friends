d6bb8ecbe6252b5f5b81543915d20b3e29a14e29
nsadisha <nsadisha@gmail.com>
2021-08-31 13:37:12 +0530

    completed

--- php/new.php
+++ php/new.php
@@ -3,30 +3,31 @@
 
 <?php
   //create a new user
-  // if(isset($_REQUEST["submit"])){
-    // getting user information
-    // $name = $_REQUEST["name"];
-    // $email = $_REQUEST["email"];
-    // $username = $_REQUEST["username"];
-    // $password = $_REQUEST["password"];
-    $name = "test";
-    $email = "test";
-    $username = "test";
-    $password = "test";
+  if(isset($_REQUEST["submit"])){
+    //getting user information
+    $name = $_REQUEST["name"];
+    $email = $_REQUEST["email"];
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["password"];
+
+    if(strlen($password)<8){
+      die("<h3>Password must have atleast 8 charactors. <a href=\"/friends/register.php\">Try again!</a></h3>");
+    }
 
     if(!isEmailUsed($email)){
-      echo "available<br>";
       $query = "INSERT INTO users(name, email, username, password) VALUES('$name', '$email', '$username', '$password')";
       $res = execute($query);
       if($res){
-        echo "Success!<br>";
+        echo "<h3>Registration successfull!!! Go to your <a href=\"/friends/index.php?username=$username\">account</a>.</h3>";
       }else{
-        echo "Failed!<br>";
+        echo "<h3>Unfortunately, your account cannot be created!</h3>";
       }
     }else{
-      echo "email found<br>";
+      echo "<h3>This email is already used. <a href=\"/friends/register.php\">Try with a new one</a>.</h3>";
     }
-  // }
+  }else{
+    echo "<h2>Please submit the form!</h2>";
+  }
 
 
 ?>
